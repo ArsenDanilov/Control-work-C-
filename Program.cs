6d13a71bce6c85d7de8487acddaf91900306b258
Arsen <ak_val@bk.ru>
2023-01-11 16:45:02 +0300

    Добавлен метод заполнения строкового массива через ввод элементов с клавиатуры

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿void FillArrayString(string[]massiv)
+{
+    int length = massiv.Length;
+    for(int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        massiv[i] = Console.ReadLine();
+    }
+}
